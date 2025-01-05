5975dcfce6c1facbf6fe1a367b0d3c8a4bce3052
aymane-fast <aymanefast@gmail.com>
2025-01-05 19:39:04 +0100

    again

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,12 @@ Route::middleware('auth:sanctum')->group(function () {
 });
 
 //testing route that return a message
-Route::get('/hello', function() {
+Route::get('/hello-testing', function() {
     return response()->json([
         'message' => 'Hello from AfrikTicket API!'
     ]);
 });
+
 // // Public Routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
